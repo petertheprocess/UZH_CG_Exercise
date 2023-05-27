a08531107bfa0d48a3e1cd0d4e3f0cddeaf68310
Tan Shaochang <shaochang.tan@uzh.ch>
2023-05-27 16:10:58 +0200

    v2

--- Exercise6/src/ray_tracer.cpp
+++ Exercise6/src/ray_tracer.cpp
@@ -1,5 +1,6 @@
 #include "ray_tracer.h"
 #include "stdlib.h"
+#include "iostream"
 #define EPSILON 0.00000001f
 // TODO: play with the samples number, add a input control in the UI (user interface, ImGui window)
 int ray_tracer::samples = 4;
@@ -34,7 +35,7 @@ glm::vec3 ray_tracer::ray_view_dir(const glm::ivec2 &pos, const glm::ivec2 &wind
 	srand(1);
 	glm::vec3 random_offset = {rand() / double(RAND_MAX), rand() / double(RAND_MAX), rand() / double(RAND_MAX)};
 
-	glm::vec3 ray_view_dir = world_space - cam_pos;
+	glm::vec3 ray_view_dir = glm::normalize(world_space - cam_pos);
 	// TODO: implement this function to compute the direction of primary rays with a random offset.
 	// HINT: do this first, and test it with the embree tracer, then complete the rt_simple class.
 	return ray_view_dir;
@@ -45,26 +46,29 @@ float rt_simple::intersect_triangle(const Triangle &tri, const glm::vec3 &org, c
 {
 	float depth = -1;
 	glm::vec3 normal = cross(tri.p1 - tri.p0, tri.p2 - tri.p0);
-	float d = -dot(normal, p1);
+	float d = -dot(normal, tri.p1);
+	// std::cout<<"d:"<<d<<std::endl;
 	if (fabsf(dot(normal, dir)) < EPSILON)
 		return depth;
 
 	float t = (-d - dot(normal, org)) / dot(normal, dir);
+	// std::cout<<"t:"<<t<<std::endl;
 	// t<0 means opposite direction of dir
 	if (t < 0)
 		return depth;
 	glm::vec3 p_intersect = org + t * dir;
 	float dis = distance(p_intersect, org);
-	if (depth != NULL && dis > depth)
+	
+	// if (depth != NULL && dis > depth)
+	// 	return depth;
+	// std::cout<<"dis:"<<dis<<std::endl;
+	if (dot(cross(tri.p1 - tri.p0, p_intersect - tri.p0), normal) < 0)
 		return depth;
-
-	if (dot(cross(p1 - p0, p_intersect - p0), normal) < 0)
+	if (dot(cross(tri.p2 - tri.p1, p_intersect - tri.p1), normal) < 0)
 		return depth;
-	if (dot(cross(p2 - p1, p_intersect - p1), normal) < 0)
+	if (dot(cross(tri.p0 - tri.p2, p_intersect - tri.p2), normal) < 0)
 		return depth;
-	if (dot(cross(p0 - p2, p_intersect - p2), normal) < 0)
-		return depth;
-
+	// std::cout<<"depth:"<<depth<<std::endl;
 	return depth = dis;
 }
 
@@ -79,82 +83,85 @@ float rt_simple::intersect_bvh(BVHNode *node, const glm::vec3 &org, const glm::v
 
 	for (int i = 0; i < 3; i++)
 	{
-		float t_min[i] = (node->boundsMin[i] - org[i]) / dir[i];
-		float t_max[i] = (node->boundsMas[i] - org[i]) / dir[i];
+		t_min[i] = (node->boundsMin[i] - org[i]) / dir[i];
+		t_max[i] = (node->boundsMax[i] - org[i]) / dir[i];
 		if (t_min[i] > t_max[i])
 			std::swap(t_max[i], t_min[i]);
 	}
 
 	float t_enter = std::max(std::max(t_min[0], t_min[1]), t_min[2]);
 	float t_exit = std::min(std::min(t_max[0], t_max[1]), t_max[2]);
-
-	if (t_exit < t_enter || t_exit <= 0)
+	// std::cout<<"t_enter"<<t_enter<<"t_exit"<<t_exit<<std::endl;
+	
+	if ( t_exit < t_enter || t_exit < 0)
+	// 不能用== 因为最后的子节点包围盒实际上是一个很薄的平面，因为只包了一个三角形
 		return depth;
-	else if (node->triangleId) // 如果包围盒里有三角形
+	else if (node->triangleId >= 0) // 如果包围盒里有三角形
 	{
-		depth = intersect_triangle(triangles[node->triangleId], org, dir);
+		Triangle tri = triangles[node->triangleId];
+		// std::cout<<node->triangleId<<","<<tri.p1.x<<std::endl;
+		float self_depth = intersect_triangle(tri, org, dir);
+		if (node->leftChild != nullptr)
+		{
+			float left_depth = intersect_bvh(node->leftChild, org, dir);
+			if (left_depth > 0 && self_depth > 0)
+			{
+				depth = std::min(left_depth, self_depth);
+			}
+			else if (left_depth < 0 && self_depth > 0)
+			{
+				depth = self_depth;
+			}
+			else
+			{
+				depth = left_depth;
+			}
+		}else{
+			depth = self_depth;
+		}
 	}
 	else
 	{
 		float left_depth = intersect_bvh(node->leftChild, org, dir);
 		float right_depth = intersect_bvh(node->rightChild, org, dir);
-		if(left_depth>0 && right_depth>0){
-			depth = std::min(left_depth,right_depth);
-		} else if(left_depth<0 && right_depth>0){
+		if (left_depth > 0 && right_depth > 0)
+		{
+			depth = std::min(left_depth, right_depth);
+		}
+		else if (left_depth < 0 && right_depth > 0)
+		{
 			depth = right_depth;
-		} else{
+		}
+		else
+		{
 			depth = left_depth;
 		}
 	}
+
 	return depth;
 }
 
 float rt_simple::intersect_depth(const glm::vec3 &org, const glm::vec3 &dir)
 {
-	float depth = intersect_bvh(rootNode,org,dir);
-	return depth>0? depth:0;
-
-	// for (auto face : faces)
-	// {
-	// 	glm::vec3 p0 = positions[face.x];
-	// 	glm::vec3 p1 = positions[face.y];
-	// 	glm::vec3 p2 = positions[face.z];
-
-	// 	glm::vec3 normal = cross(p1 - p0, p2 - p0);
-	// 	float d = -dot(normal, p1);
-	// 	if (fabsf(dot(normal, dir)) < EPSILON)
-	// 		continue;
-
-	// 	float t = (-d - dot(normal, org)) / dot(normal, dir);
-	// 	// t<0 means opposite direction of dir
-	// 	if (t < 0)
-	// 		continue;
-	// 	glm::vec3 p_intersect = org + t * dir;
-	// 	float dis = distance(p_intersect, org);
-	// 	if (depth != NULL && dis > depth)
-	// 		continue;
-
-	// 	if (dot(cross(p1 - p0, p_intersect - p0), normal) < 0)
-	// 		continue;
-	// 	if (dot(cross(p2 - p1, p_intersect - p1), normal) < 0)
-	// 		continue;
-	// 	if (dot(cross(p0 - p2, p_intersect - p2), normal) < 0)
-	// 		continue;
-
-	// 	depth = dis;
-	// }
-
-	// return depth;
+	float depth = intersect_bvh(rootNode, org, dir);
+	// std::cout<<"depth:"<<depth<<std::endl;
+	return depth > 0 ? depth : 0;
 };
 
 unsigned rt_simple::add_mesh(const Shape &mesh)
 {
 	model_matrix = mesh.getModelMatrix();
-
-	Triangle tri;
-	tri.p0 = glm::vec3(model_matrix * glm::vec4(mesh.positions[face[0]], 1.0f));
-	tri.p1 = glm::vec3(model_matrix * glm::vec4(mesh.positions[face[0]], 1.0f));
-	tri.p2 = glm::vec3(model_matrix * glm::vec4(mesh.positions[face[0]], 1.0f));
-	triangles.push_back(tri);
+	unsigned count = 0;
+	for (auto face : mesh.faces)
+	{
+		Triangle tri;
+		tri.p0 = glm::vec3(model_matrix * glm::vec4(mesh.positions[face[0]], 1.0f));
+		tri.p1 = glm::vec3(model_matrix * glm::vec4(mesh.positions[face[1]], 1.0f));
+		tri.p2 = glm::vec3(model_matrix * glm::vec4(mesh.positions[face[2]], 1.0f));
+		triangles.push_back(tri);
+		// std::cout<<triangles[count].p0.x<<std::endl;
+		count++;
+	}
+	return count;
 }
 // TODO: add the definition of the methods here.
